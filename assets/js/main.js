9d76df8b105673c768b45f2ffc6cbfb0454134c5
Dejan.Angelovski-FE11 <paradoxangelov@yahoo.com>
2023-04-17 14:18:16 +0200

    Start 17.04.2023
    Setting up necessary folders
    starting html ,css ,main js,
    Added Nav bar and Banner section

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// navbar offcanvas
+
+$(function () {
+    'use strict'
+
+    $('[data-toggle="offcanvas"]').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+})
