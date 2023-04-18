c2708d6a64e7bf15cd109c860c109e34bd8decf7
Dejan.Angelovski-FE11 <paradoxangelov@yahoo.com>
2023-04-18 20:27:00 +0200

    Update 18.04.2023
    Done with filter section and with cards for marketing coding and design ,created a show more button but its still a work in progress for now .

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,93 @@ $(function () {
     })
 
 })
+
+// filters
+
+document.querySelector("#filter-marketing").addEventListener("change", filterMarketing);
+document.querySelector("#filter-coding").addEventListener("change", filterCoding);
+document.querySelector("#filter-design").addEventListener("change", filterDesign);
+
+function filterMarketing() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-marketing").checked) {
+        var marketingCards = document.querySelectorAll(".marketing");
+
+        marketingCards.forEach(marketing => {
+            marketing.style.display = "block";
+        });
+
+        document.querySelector("#filter-coding").checked = false;
+        document.querySelector("#filter-design").checked = false;
+    } else {
+        showAllCards();
+    };
+}
+
+function filterCoding() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-coding").checked) {
+        var codingCards = document.querySelectorAll(".coding");
+
+        codingCards.forEach(coding => {
+            coding.style.display = "block";
+        });
+
+        document.querySelector("#filter-design").checked = false;
+        document.querySelector("#filter-marketing").checked = false;
+    } else {
+        showAllCards();
+    };
+}
+
+function filterDesign() {
+    hideAllCards();
+
+    if(document.querySelector("#filter-design").checked) {
+        var designCards = document.querySelectorAll(".design");
+
+        designCards.forEach(design => {
+            design.style.display = "block";
+        });
+
+        document.querySelector("#filter-marketing").checked = false;
+        document.querySelector("#filter-coding").checked = false;
+    } else {
+        showAllCards();
+    };
+}
+
+function hideAllCards() {
+    var allCards = document.querySelectorAll(".project, #loadMore");
+
+    allCards.forEach(project => {
+        project.style.display = "none";
+    });
+}
+
+function showAllCards() {
+    var allCards = document.querySelectorAll(".project, #loadMore");
+
+    allCards.forEach(project => {
+        project.style.display = "block";
+    });
+}
+
+
+// Show more button
+
+$(document).ready(function () {
+    $(".project").slice(0, 3).show();
+    if ($(".card:hidden").length != 0) {
+        $("#loadMore").show();
+    }
+    $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".project:hidden").slice(0, 6).slideDown();
+        if ($(".project:hidden").length == 0) {
+            $("#loadMore").fadeOut('slow');
+        }
+    });
+});
